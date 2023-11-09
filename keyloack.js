24da7cf5d663727c2b8d04dc63bddb9889115571
Lujain Sallam <142907139+LujainSallam1@users.noreply.github.com>
2023-11-09 15:38:05 +0100

    Update keyloack.js

--- keyloack.js
+++ keyloack.js
@@ -18,7 +18,7 @@ const keycloak = Keycloak({
         id_token_hint: idToken, // إرسال ID Token كمعلمة
         post_logout_redirect_uri: postLogoutRedirect,
      });
-     keycloak.logout({logoutURL});
+     window.location.href = logoutUrl;
   });
   
   keycloak
