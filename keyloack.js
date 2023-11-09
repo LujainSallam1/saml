5129eff4cbd466b8f6eb514d437cfab9a6f7d8a6
Lujain Sallam <142907139+LujainSallam1@users.noreply.github.com>
2023-11-09 14:03:26 +0100

    Update keyloack.js

--- keyloack.js
+++ keyloack.js
@@ -1,6 +1,50 @@
+const keycloak = Keycloak({
+    url: 'http://localhost:8080',
+    realm: 'master',
+    clientId: 'frontend',
+    redirectUri: 'https://lujainsallam1.github.io/saml',
+       enableDebug: true,
+  });
+  
+  document.getElementById('login').addEventListener('click', () => {
+    keycloak.login();
+  });
+  
+  document.getElementById('logout').addEventListener('click', () => {
+       const idToken = keycloak.token;
+      console.log(idToken);
+     const postLogoutRedirect = 'https://lujainsallam1.github.io/saml';
+       console.log(postLogoutRedirect);
 
-document.getElementById('logout').addEventListener('click', () => {
-  keycloak.logout();
-});
+       keycloak.logout({ post_logout_redirect_uri: postLogoutRedirect, id_token_hint: idToken });
+  });
+  
+  keycloak
+    .init({ onLoad: 'login-required' })
+    .then((authenticated) => {
+      if (authenticated) {
+        const accessToken = keycloak.token;
+        console.log(`Access Token: ${accessToken}`);
+  
+        // Check if the user has the "admin" role
+       const tokenParsed = keycloak.tokenParsed;
+        const roles = tokenParsed.realm_access.roles;
+  
 
+        if (roles.includes("admin")) {
+          console.log("User has 'admin' role");
+        } else {
+          alert("User does not have admin role. Access denied.");
+          keycloak.logout();
+        }
+        console.log("Access Token:", keycloak.token);
 
+      }
+   else {
+        alert("User authentication failed!");
+      }
+    })
+    .catch(() => {
+      alert("Could not authenticate the user!");
+    });
+  
