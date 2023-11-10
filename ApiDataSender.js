0264fefdf850fab98cd3db15fd9bd04455440b05
Lujain Sallam <142907139+LujainSallam1@users.noreply.github.com>
2023-11-10 15:07:23 +0100

    Update ApiDataSender.js

--- ApiDataSender.js
+++ ApiDataSender.js
@@ -104,6 +104,20 @@ buttonInput.addEventListener('click', () => {
             "principalType": principalType,
         }
     };
+    
+    for (const key in data) {
+        if (!data[key]) {
+            delete data[key];
+        }
+    }
+
+    // التحقق من قيمة الحقول
+    for (const key in data) {
+        if (typeof data[key] === 'string' && !data[key].trim()) {
+            console.error(`حقل ${key} يجب أن يحتوي على قيمة.`);
+            return;
+        }
+    }
 
     fetch(url, {
         method: 'POST',
