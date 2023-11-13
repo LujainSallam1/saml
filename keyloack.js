072b9480e3c20fba8a9c3ecf1ab29085bb5f7785
Lujain Sallam <142907139+LujainSallam1@users.noreply.github.com>
2023-11-13 11:22:43 +0100

    Update keyloack.js

--- keyloack.js
+++ keyloack.js
@@ -1,44 +1,42 @@
 let accessToken;
 const keycloak = Keycloak({
-    url: 'http://localhost:8080',
-    realm: 'master',
-    clientId: 'frontend',
-    redirectUri: 'https://lujainsallam1.github.io/saml',
-    enableDebug: true,
-    
-  });
-  
-  document.getElementById('login').addEventListener('click', () => {
-    keycloak.login();
-  });
-  
-  document.getElementById('logout').addEventListener('click', () => {
-     const clientid='frontend';
-     const postLogoutRedirect = 'https://lujainsallam1.github.io/saml/';
-     window.location.href =`http://localhost:8080/realms/master/protocol/openid-connect/logout?post_logout_redirect_uri=${postLogoutRedirect}&client_id=${clientid}`;
-  });
-  
-  keycloak
-    .init({ onLoad: 'login-required' })
-    .then((authenticated) => {
-      if (authenticated) {
-        accessToken = keycloak.token;
-        console.log(`Access Token: ${accessToken}`);
-  
-        // Check if the user has the "admin" role
-       const tokenParsed = keycloak.tokenParsed;
-        const roles = tokenParsed.realm_access.roles;
-  
+  url: 'http://localhost:8080',
+  realm: 'master',
+  clientId: 'frontend',
+  redirectUri: 'https://lujainsallam1.github.io/saml',
+  enableDebug: true,
+});
 
-        if (roles.includes("admin")) {
-          console.log("User has 'admin' role");
-        } else {
-          alert("User does not have admin role. Access denied.");
-             const clientid='frontend';
-             const postLogoutRedirect = 'https://lujainsallam1.github.io/saml/';
-             window.location.href =`http://localhost:8080/realms/master/protocol/openid-connect/logout?post_logout_redirect_uri=${postLogoutRedirect}&client_id=${clientid}`;
-         
-    // Set interval for token refresh
+document.getElementById('login').addEventListener('click', () => {
+  keycloak.login();
+});
+
+document.getElementById('logout').addEventListener('click', () => {
+  const clientid = 'frontend';
+  const postLogoutRedirect = 'https://lujainsallam1.github.io/saml/';
+  window.location.href = `http://localhost:8080/realms/master/protocol/openid-connect/logout?post_logout_redirect_uri=${postLogoutRedirect}&client_id=${clientid}`;
+});
+
+keycloak
+  .init({ onLoad: 'login-required' })
+  .then((authenticated) => {
+    if (authenticated) {
+      accessToken = keycloak.token;
+      console.log(`Access Token: ${accessToken}`);
+  
+      // Check if the user has the "admin" role
+      const tokenParsed = keycloak.tokenParsed;
+      const roles = tokenParsed.realm_access.roles;
+  
+      if (roles.includes("admin")) {
+        console.log("User has 'admin' role");
+      } else {
+        alert("User does not have admin role. Access denied.");
+        const clientid = 'frontend';
+        const postLogoutRedirect = 'https://lujainsallam1.github.io/saml/';
+        window.location.href = `http://localhost:8080/realms/master/protocol/openid-connect/logout?post_logout_redirect_uri=${postLogoutRedirect}&client_id=${clientid}`;
+        
+        // Set interval for token refresh
         setInterval(() => {
           keycloak.updateToken(180).then((bool) => {
             if (bool) {
@@ -48,8 +46,6 @@ const keycloak = Keycloak({
             }
           });
         }, 126000);
-      } else {
-        alert("User authentication failed!");
       }
     } else {
       alert("User authentication failed!");
